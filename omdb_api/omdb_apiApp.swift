cea7eb3c7b47f4fff5d51e34d3493e444129dc4c
Kaan İzgi <kaanizgi1531@gmail.com>
2022-08-15 17:47:01 +0300

    Initial Commit

---
+++ omdb_api/omdb_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  omdb_apiApp.swift
+//  omdb_api
+//
+//  Created by Kaan İzgi on 15.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct omdb_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
